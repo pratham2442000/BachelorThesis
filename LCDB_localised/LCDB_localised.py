40d95d9acb3a489fdee62c5af20e1af25422a436
Pratham Johari <pratham244200@gmail.com>
2024-01-09 18:29:06 +0100

    Update preprocessing to use test instead of validation dataset

--- LCDB_localised/LCDB_localised.py
+++ LCDB_localised/LCDB_localised.py
@@ -4,17 +4,16 @@ import pandas as pd
 import os
 
 a = lcdb.get_all_curves()
-pd.to_pickle(a, 'all_curves.pkl')
+pd.to_pickle(a, '/mnt/c/Users/prath/PycharmProjects/rp/LCDB_localised/all_curves.pkl')
 # read from pickle
 path = os.path.dirname(os.path.abspath(__file__))
 a = pd.read_pickle(path + '/all_curves.pkl')
 
+print(path)
+print("start")
 
 # create a dict of all pair of openmlid and learner
 openmlid_learner = {}
-
-print("start")
-
 for i in range(len(a)):
     #     i , openmlid, learner
     print('\r', i, end='')
@@ -24,18 +23,21 @@ for i in range(len(a)):
     else:
         openmlid_learner[a.iloc[i,0]] = [a.iloc[i,1]]
 
-# print(openmlid_learner)
+print(openmlid_learner)
 # write dic to file
-with open('openmlid_learner.txt', 'w') as f:
+with open('/mnt/c/Users/prath/PycharmProjects/rp/LCDB_localised/openmlid_learner.txt', 'w') as f:
     f.write(str(openmlid_learner))
-
 # read from file
-with open(path + '/openmlid_learner.txt', 'r') as f:
+with open('/mnt/c/Users/prath/PycharmProjects/rp/LCDB_localised/openmlid_learner.txt', 'r') as f:
     openmlid_learner = eval(f.read())
 
 
 print(sum([len(i) for i in openmlid_learner.values()]))
 print(a.info())
+
+
+#---------------------------------------------------------------------------------------------------
+
 
 def get_curve(rng:np.random, verbose=False):
     # get a random openmlid
@@ -51,7 +53,6 @@ def get_curve(rng:np.random, verbose=False):
         print(f"openmlid: {openmlid}, learner: {learner}")
     return get_curve_from_dataframe(get_curve_as_dataframe(openmlid, learner))
 
-
 def get_curve_as_dataframe(openmlid, learner):
 
     df = a.copy()
@@ -62,7 +63,6 @@ def get_curve_as_dataframe(openmlid, learner):
     if len(df) == 0:
         raise Exception(f"No curves found for learner {learner} on openmlid {openmlid}")
     return df
-
 
 def get_curve_from_dataframe(curve_df):
     # sanity check to see that, if we know the learner, that there is only one of them
@@ -86,7 +86,6 @@ def get_curve_from_dataframe(curve_df):
 
     return anchors, values_train, values_valid, values_test
 
-
 #for each openmlid and leaner, get an anchor, values_train, values_valid, values_test and save it for faster access
 def get_all_curves():
     all_curves = []
@@ -94,25 +93,32 @@ def get_all_curves():
         for learner in openmlid_learner[openmlid]:
             print(f"openmlid: {openmlid}, learner: {learner}")
             anchors, values_train, values_valid, values_test = get_curve_from_dataframe(get_curve_as_dataframe(openmlid, learner))
-            means = [np.mean(sublist) for sublist in values_valid]
+            means = [np.mean(sublist) for sublist in values_test]
             all_curves.append([openmlid, learner, anchors, means, np.std([max(sublist) - min(sublist) for sublist in values_valid])])
     all_curves = pd.DataFrame(all_curves, columns=['openmlid', 'learner', 'anchors', 'means', 'std'])
     return all_curves
 
 a = get_all_curves()
+a.to_pickle('/mnt/c/Users/prath/PycharmProjects/rp/LCDB_localised/all_curves_preprocessed.pkl')
 
 print(a.info())
 print(a.head())
 print(a.tail())
 print(a.describe())
 
-df = pd.read_pickle('all_curves_preprocessed.pkl')
 
-#split the data into train, test
+
+#---------------------------------------------------------------------------------------------------
+
+
+
+
+df = pd.read_pickle('/mnt/c/Users/prath/PycharmProjects/rp/LCDB_localised/all_curves_preprocessed.pkl')
 from sklearn.model_selection import train_test_split
 train, test = train_test_split(df, test_size=0.2, random_state=42)
 print(train.info())
 print(test.info())
 #save the data
-train.to_pickle('train_curves.pkl')
-test.to_pickle('test_curves.pkl')
+train.to_pickle('/mnt/c/Users/prath/PycharmProjects/rp/LCDB_localised/train_curves.pkl')
+test.to_pickle('/mnt/c/Users/prath/PycharmProjects/rp/LCDB_localised/test_curves.pkl')
+
