5898318552968582bd2fda9cb421cf94ef5ed27e
Pratham Johari <pratham244200@gmail.com>
2024-01-15 11:05:19 +0100

    remove files not need for thesis

--- LCDB_localised/LCDB_localised.py
+++ LCDB_localised/LCDB_localised.py
@@ -1,7 +1,7 @@
 import lcdb
 import numpy as np
-import pandas as pd
 import os
+import pandas as pd
 
 a = lcdb.get_all_curves()
 print(a.info())
@@ -19,11 +19,11 @@ openmlid_learner = {}
 for i in range(len(a)):
     #     i , openmlid, learner
     print('\r', i, end='')
-    if a.iloc[i,0] in openmlid_learner:
-        if a.iloc[i,1] not in openmlid_learner[a.iloc[i,0]]:
-            openmlid_learner[a.iloc[i,0]].append(a.iloc[i,1])
+    if a.iloc[i, 0] in openmlid_learner:
+        if a.iloc[i, 1] not in openmlid_learner[a.iloc[i, 0]]:
+            openmlid_learner[a.iloc[i, 0]].append(a.iloc[i, 1])
     else:
-        openmlid_learner[a.iloc[i,0]] = [a.iloc[i,1]]
+        openmlid_learner[a.iloc[i, 0]] = [a.iloc[i, 1]]
 
 print(openmlid_learner)
 # write dic to file
@@ -33,15 +33,14 @@ with open('/mnt/c/Users/prath/PycharmProjects/rp/LCDB_localised/openmlid_learner
 with open('/mnt/c/Users/prath/PycharmProjects/rp/LCDB_localised/openmlid_learner.txt', 'r') as f:
     openmlid_learner = eval(f.read())
 
-
 print(sum([len(i) for i in openmlid_learner.values()]))
 print(a.info())
 
 
-#---------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------
 
 
-def get_curve(rng:np.random, verbose=False):
+def get_curve(rng: np.random, verbose=False):
     # get a random openmlid
     openmlid = rng.choice(list(openmlid_learner.keys()))
     # get a random learner
@@ -55,8 +54,8 @@ def get_curve(rng:np.random, verbose=False):
         print(f"openmlid: {openmlid}, learner: {learner}")
     return get_curve_from_dataframe(get_curve_as_dataframe(openmlid, learner))
 
-def get_curve_as_dataframe(openmlid, learner):
 
+def get_curve_as_dataframe(openmlid, learner):
     df = a.copy()
     df = df[(df["openmlid"] == openmlid)]
     if len(df) == 0:
@@ -65,6 +64,7 @@ def get_curve_as_dataframe(openmlid, learner):
     if len(df) == 0:
         raise Exception(f"No curves found for learner {learner} on openmlid {openmlid}")
     return df
+
 
 def get_curve_from_dataframe(curve_df):
     # sanity check to see that, if we know the learner, that there is only one of them
@@ -88,17 +88,21 @@ def get_curve_from_dataframe(curve_df):
 
     return anchors, values_train, values_valid, values_test
 
-#for each openmlid and leaner, get an anchor, values_train, values_valid, values_test and save it for faster access
+
+# for each openmlid and leaner, get an anchor, values_train, values_valid, values_test and save it for faster access
 def get_all_curves():
     all_curves = []
     for openmlid in openmlid_learner.keys():
         for learner in openmlid_learner[openmlid]:
             print(f"openmlid: {openmlid}, learner: {learner}")
-            anchors, values_train, values_valid, values_test = get_curve_from_dataframe(get_curve_as_dataframe(openmlid, learner))
+            anchors, values_train, values_valid, values_test = get_curve_from_dataframe(
+                get_curve_as_dataframe(openmlid, learner))
             means = [np.mean(sublist) for sublist in values_valid]
-            all_curves.append([openmlid, learner, anchors, means, np.std([max(sublist) - min(sublist) for sublist in values_valid])])
+            all_curves.append(
+                [openmlid, learner, anchors, means, np.std([max(sublist) - min(sublist) for sublist in values_valid])])
     all_curves = pd.DataFrame(all_curves, columns=['openmlid', 'learner', 'anchors', 'means', 'std'])
     return all_curves
+
 
 a = get_all_curves()
 a.to_pickle('/mnt/c/Users/prath/PycharmProjects/rp/LCDB_localised/all_curves_preprocessed.pkl')
@@ -108,19 +112,15 @@ print(a.head())
 print(a.tail())
 print(a.describe())
 
-
-
-#---------------------------------------------------------------------------------------------------
-
-
+# ---------------------------------------------------------------------------------------------------
 
 
 df = pd.read_pickle('/mnt/c/Users/prath/PycharmProjects/rp/LCDB_localised/all_curves_preprocessed.pkl')
 from sklearn.model_selection import train_test_split
+
 train, test = train_test_split(df, test_size=0.2, random_state=42)
 print(train.info())
 print(test.info())
-#save the data
+# save the data
 train.to_pickle('/mnt/c/Users/prath/PycharmProjects/rp/LCDB_localised/train_curves.pkl')
 test.to_pickle('/mnt/c/Users/prath/PycharmProjects/rp/LCDB_localised/test_curves.pkl')
-
